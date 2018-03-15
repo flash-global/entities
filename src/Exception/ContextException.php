c819d795550b5f67632dd6cca7ca292df12cc4cf
Rwellens <r.wellens@flash-global.net>
2018-03-15 11:48:00 +0100

    merge develop

---
+++ src/Exception/ContextException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gauthier
+ * Date: 09/08/2017
+ * Time: 10:14
+ */
+
+namespace Fei\Entities\Exception;
+
+
+class ContextException extends \Exception
+{
+    
+}
